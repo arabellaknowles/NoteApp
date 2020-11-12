6cdaac09c708711490a2b99404976221f4d0c24b
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-12 09:50:58 +0000

    Initial commit to new remote repository

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -7,7 +7,6 @@ class NoteListView {
     let htmlOutput = "";
     for (let i = 0; i < this.list.length; i++) {
       var limitedNote = this.list[i].text().substring(0, 20)
-      console.log(limitedNote)
       htmlOutput += `<li><a href='#notes/${i}'>${limitedNote}</a></li>`
     }
     return "<ul>" + htmlOutput + "</ul>"
